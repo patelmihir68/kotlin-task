11561fd737283727bffc1d01b4411ce4f4cf6950
Mihir Patel <69503728+patelmihir68@users.noreply.github.com>
2022-04-18 21:26:59 +0530

    Update task2.kt

--- task2.kt
+++ task2.kt
@@ -64,7 +64,7 @@ class Coffee : Beverage(), CleanPot {
 }
 
 fun main(args: Array<String>) {
-    println("Want Beverage? Enter 1 for Tea / 2 for Coffee!")
+    println("Want beverage? Enter 1 for Tea / 2 for Coffee!")
     println(args[0])
     if (args[0].equals("1")) {
         var baverage = Tea()
