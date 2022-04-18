cecf24c0cba413ddca8ef1035ef053e8c4d2de81
Mihir Patel <69503728+patelmihir68@users.noreply.github.com>
2022-04-18 21:22:43 +0530

    Create task2.kt

---
+++ task2.kt
@@ -0,0 +1,78 @@
+interface CleanPot {
+    fun cleanPot()
+}
+
+abstract class Beverage {
+
+    fun addHotWater() {
+        println("Adding hot water")
+    }
+
+    fun addHotMilk() {
+        println("Adding hot milk")
+    }
+
+    fun addSugar() {
+        println("Adding sugar")
+    }
+
+    fun getMixture() {
+        addHotWater()
+        addHotMilk()
+        addSugar()
+    }
+
+    abstract fun getBeverage()
+
+    abstract fun addIngredients()
+}
+
+class Tea : Beverage(), CleanPot {
+
+    override fun cleanPot() {
+        println("Cleaning  pot")
+    }
+
+    override fun addIngredients() {
+        println("Tea bag is added")
+    }
+
+    override fun getBeverage() {
+        cleanPot()
+        getMixture()
+        addIngredients()
+        println("Tea is Ready! Enjoy")
+    }
+}
+
+class Coffee : Beverage(), CleanPot {
+
+    override fun cleanPot() {
+        println("Cleaning  pot")
+    }
+
+    override fun addIngredients() {
+        println("Coffee bag is added")
+    }
+    
+    override fun getBeverage() {
+        cleanPot()
+        getMixture()
+        addIngredients()
+        println("Coffee is Ready! Enjoy")
+    }
+}
+
+fun main(args: Array<String>) {
+    println("Want Beverage? Enter 1 for Tea / 2 for Coffee!")
+    println(args[0])
+    if (args[0].equals("1")) {
+        val baverage = Tea()
+        baverage.getBeverage()
+    }else if (args[0].equals("2")) {
+        var baverage = Coffee()
+        baverage.getBeverage()
+    }else{
+        println("Invalid Input")
+    }
+}
